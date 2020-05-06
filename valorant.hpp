54a5db2bbf7760b763d768820c9c456987b5873a
apekros <43776524+apekros@users.noreply.github.com>
2020-05-06 22:43:09 +0930

    Update valorant.hpp

--- valorant.hpp
+++ valorant.hpp
@@ -12,8 +12,10 @@ Version: 0.49
 std::ptrdiff_t engine = 0x61E9AF0;
 std::ptrdiff_t world = 0x61EC228;
 
-std::ptrdiff_t korean_world = 0x61EE2A8;
-
+#ifdef KOREA
+world = 0x61EE2A8;
+engine = 0x61EBB70;
+#endif
 
 std::ptrdiff_t root_component = 0x238;
 std::ptrdiff_t player_state = 0x3c8;
