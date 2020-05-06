3b958f5a7a4fb0d23f15aba105f0a7f751bb5b03
apekros <43776524+apekros@users.noreply.github.com>
2020-05-06 16:21:15 +0930

    0.49

---
+++ valorant.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstddef>
+
+/*
+Valorant Offsets Repository @apekros
+Timestamp: Wed May  6 16:17:53 2020
+Version: 0.49*/
+
+std::ptrdiff_t root_component = 0x238;
+std::ptrdiff_t player_state = 0x3c8;
+std::ptrdiff_t team_component = 0x5b0;
+std::ptrdiff_t team = 0x118;
+std::ptrdiff_t damage_handler = 0xa20;
+std::ptrdiff_t cached_life = 0x190;
+std::ptrdiff_t relative_location = 0x184;
+std::ptrdiff_t relative_rotation = 0x190;
