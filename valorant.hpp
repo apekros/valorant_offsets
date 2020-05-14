f37eefc3c114fe2ef4cca5effff00116992b7ca5
apekros <43776524+apekros@users.noreply.github.com>
2020-05-14 14:37:19 +0930

    all regions same now

--- valorant.hpp
+++ valorant.hpp
@@ -10,10 +10,10 @@ Version: 0.49/0.50 NA
 /*
 Globals
 */
-std::ptrdiff_t g_engine = 0x61ebb70;
-std::ptrdiff_t world = 0x61ee2a8;
-std::ptrdiff_t g_objects = 0x60c7538;
-std::ptrdiff_t g_names = 0x6090000;
+std::ptrdiff_t g_engine = 0x627a7a0;
+std::ptrdiff_t world = 0x627ced8;
+std::ptrdiff_t g_objects = 0x6156138;
+std::ptrdiff_t g_names = 0x611ec00;
 
 
 /*
@@ -25,29 +25,17 @@ std::ptrdiff_t owning_game_instance = 0x190;
 std::ptrdiff_t player_state = 0x3c8;
 std::ptrdiff_t team_component = 0x5b0;
 std::ptrdiff_t team = 0x118;
-std::ptrdiff_t damage_handler = 0xa20;
+std::ptrdiff_t damage_handler = 0xa50;
 std::ptrdiff_t cached_life = 0x190;
 std::ptrdiff_t relative_location = 0x184;
 std::ptrdiff_t relative_rotation = 0x190;
 std::ptrdiff_t mesh_component = 0x408;
-std::ptrdiff_t static_mesh = 0x528;
+std::ptrdiff_t static_mesh = 0x548;
 /*
 Other
 */
 std::ptrdiff_t actor_count = 0xA8;
 std::ptrdiff_t actor_data = 0xA0;
 
-std::ptrdiff_t fn_free = 0x1B759B0;
+std::ptrdiff_t fn_free = 0x1BCDBB0;
 
-/*
-Regions
-*/
-#ifdef NA
-g_engine = 0x627a7a0;
-world = 0x627ced8;
-g_objects = 0x6156138;
-g_names = 0x611ec00;
-damage_handler = 0xa50;
-static_mesh = 0x548;
-fn_free = 0x1BCDBB0;
-#endif
