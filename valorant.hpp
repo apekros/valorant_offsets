e2ec655b0a5488c73646feca2dba1fac6f2e6553
Bayley Foster <43776524+apekros@users.noreply.github.com>
2021-02-18 16:20:32 +1030

    2.03 - patch

--- valorant.hpp
+++ valorant.hpp
@@ -3,7 +3,7 @@
 
 /*
 Valorant Offsets Repository @apekros
-Timestamp: Wed Feb  3 17:38:13 2021
+Timestamp: Thu Feb 18 16:18:24 2021
 Version: 4.24.3.0
 */
 
@@ -14,11 +14,10 @@ std::ptrdiff_t mesh_component = 0x518;
 std::ptrdiff_t root_component = 0x210;
 std::ptrdiff_t persistent_level = 0x38;
 std::ptrdiff_t owning_game_instance = 0x190;
-std::ptrdiff_t player_state = 0x3a0;
-std::ptrdiff_t team_component = 0x570;
+std::ptrdiff_t player_state = 0x3c8;
+std::ptrdiff_t team_component = 0x598;
 std::ptrdiff_t team = 0xf0;
-std::ptrdiff_t damage_handler = 0xa70;
+std::ptrdiff_t damage_handler = 0xaa0;
 std::ptrdiff_t cached_life = 0x188;
 std::ptrdiff_t relative_location = 0x15c;
 std::ptrdiff_t relation_rotation = 0x168;
-
