392c7ce20cf2f01713b79efe0814545a7c5fe080
Bayley Foster <43776524+apekros@users.noreply.github.com>
2021-06-10 13:17:33 +0930

    2.11
    sorry for the delay had no access to the repo!

--- valorant.hpp
+++ valorant.hpp
@@ -3,26 +3,26 @@
 
 /*
 Valorant Offsets Repository @apekros
-Timestamp: Wed May 26 15:26:21 2021
-Version: 4.24.3.0
+Timestamp: Wed Jun  9 22:14:16 2021
+Version: 4.25.4.0
 */
 
 /*
 Classes
 */
-std::ptrdiff_t mesh_component = 0x520;
+std::ptrdiff_t mesh_component = 0x550;
 std::ptrdiff_t root_component = 0x210;
 std::ptrdiff_t persistent_level = 0x38;
-std::ptrdiff_t owning_game_instance = 0x190;
-std::ptrdiff_t player_state = 0x3c8;
-std::ptrdiff_t team_component = 0x598;
-std::ptrdiff_t team = 0xf0;
-std::ptrdiff_t damage_handler = 0xab8;
-std::ptrdiff_t cached_life = 0x188;
-std::ptrdiff_t relative_location = 0x15c;
-std::ptrdiff_t relative_rotation = 0x168;
+std::ptrdiff_t owning_game_instance = 0x1a8;
+std::ptrdiff_t player_state = 0x3d0;
+std::ptrdiff_t team_component = 0x580;
+std::ptrdiff_t team = 0xf8;
+std::ptrdiff_t damage_handler = 0x940;
+std::ptrdiff_t cached_life = 0x190;
+std::ptrdiff_t relative_location = 0x164;
+std::ptrdiff_t relative_rotation = 0x170;
 std::ptrdiff_t player_controller = 0x38;
-std::ptrdiff_t acknowledged_pawn = 0x438;
-std::ptrdiff_t player_camera_manager = 0x450;
-std::ptrdiff_t camera_cache_private = 0x1ee0;
-std::ptrdiff_t control_rotation = 0x418;
+std::ptrdiff_t acknowledged_pawn = 0x440;
+std::ptrdiff_t player_camera_manager = 0x458;
+std::ptrdiff_t camera_cache_private = 0x1f80;
+std::ptrdiff_t control_rotation = 0x420;
